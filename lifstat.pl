5220e58d83c0024ae9371225d22a1909b30f5ea9
Robin Peter <rpeter@Robins-MBP.local>
2017-06-13 11:18:25 -0700

    adding username & password field

--- lifstat.pl
+++ lifstat.pl
@@ -2,32 +2,37 @@
 #
 # Written By    : <Robin.Peter@gmail.com>
 # Created On    : Mon Sep  8 12:05:41 PDT 2014
-# Modified On	: Mon Sep  8 12:05:41 PDT 2014
+# Modified On	: Tue Jun 13 11:08:24 PDT 2017
 # Program Name  : lifstat.pl
-# Version       : 1.0.0
+# Version       : 1.1.0
 #
 #------------------------------------------------------------------#
 use strict;
+use Socket;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev bundling);
-use lib "/lib/NetApp";
+use lib "./lib/NetApp";
 use Math::Round qw(:all);
 use NaServer;
 use NaElement;
 #------------------------------------------------------------------#
 my $interval = 2;
-my ($filer, $node, $vserver, $help, $version);
-GetOptions("f|c|cluster=s"      => \$filer,
-           "n|node=s"           => \$node,
-           "v|vserver=s"        => \$vserver,
-           "i|interval=i"       => \$interval,
-           "h|help"             => \$help,
-           "V|version"          => \$version)
+my ($cluster, $node, $optuser, $optpasswd, $vserver, $help, $version);
+GetOptions("c|cluster=s"    => \$cluster,
+           "n|node=s"       => \$node,
+           "u|username=s"   => \$optuser,
+           "p|passwd=s"     => \$optpasswd,
+           "v|vserver=s"    => \$vserver,
+           "i|interval=i"   => \$interval,
+           "h|help"         => \$help,
+           "V|version"      => \$version)
     or die("Error In Command Line Arguments!\n", help());
 #------------------------------------------------------------------#
 sub help {
     print "Usage :: \n";
-    print "  -f|-c|--cluster : Cluster Name to get Logical Interface Status. \n";
+    print "  -c|--cluster : Cluster Name to get Logical Interface Status. \n";
     print "  -n|--node       : Node Name [To limit the lif stats to node level]. \n";
+    print "  -u|--username   : Username to connect to cluster.       Example : -u admin \n";
+    print "  -p|--passwd     : Password for username.                Example : -p netapp123 \n";
     print "  -v|--vserver    : Vserver Name [To limit the lif stats to vserver level]. \n";
     print "  -i|--interval   : The Interval in seconds between the stats. \n";
     print "  -h|--help       : Print this Help and Exit! \n";
@@ -49,49 +54,59 @@ sub help {
 if ($help eq '1'){
     help();
 } elsif ($version eq '1'){
-    print "lifstat 1.0.0 : Mon Sep  8 12:05:41 PDT 2014 \n";
+    print "lifstat 1.1.0 : Tue Jun 13 11:08:24 PDT 2017 \n";
     exit 0;
-} elsif ($filer) {
-    lifstatloop($filer);
+} elsif ($cluster) {
+    lifstatloop($cluster);
+    #getversions($cluster) #Only for Testing
 } else {
     help();
 }
 #------------------------------------------------------------------#
 sub connect_netapp {
-    my $filer   = shift;
-    my $fi1er   = NaServer->new($filer, 1, 20);
-    if ( !defined ($filer) ) {
-        my $outa    = $filer->set_server_type("FILER");
-        my $reason  = $outa->results_reason();
-        print "Unable to set server type 'FILER'. $reason\n";
-        exit 2;
-	}
-    if ( !defined ($filer) ) {
-        my $outb    = $filer->set_style("HOSTS");
-        my $reason  = $outb->results_reason();
-        print "Unable to set auth style 'HOSTS'. $reason\n";
-        exit 2;
+    my $host    = shift;
+    my $ncs     = NaServer->new ($host, 1, 3);
+    if ( (eval { $ncs->get_http_version() }) && ( $ncs->get_http_version() eq '1.1') ){
+        if ( ! gethostbyaddr( inet_aton($host), AF_INET )){
+            $ncs->set_http_version('1.0');
+        }
     }
-    if ( !defined ($filer) ) {
-        my $outc    = $filer->set_transport_type("HTTP");
-        my $reason  = $outc->results_reason();
-        print "Unable to set transport type 'HTTP'. $reason\n";
-        exit 2;
+    my $out = $ncs->set_transport_type("HTTPS");
+    if (ref ($out) eq "NaElement" && $out->results_errno != 0) {
+        $out->results_reason(); exit (-2);
     }
-    if ( !defined ($filer) ) {
-        my $outd    = $filer->set_timeout(60);
-        my $reason  = $outd->results_reason();
-        print "Unable to set connection timeout '60'. $reason\n";
-        exit 2;
+    $out = $ncs->set_port('443');
+    if (ref ($out) eq "NaElement" && $out->results_errno != 0) {
+        $out->results_reason(); exit (-2);
     }
-    return $fi1er;
+    $out = $ncs->set_style("LOGIN");
+    if (ref ($out) eq "NaElement" && $out->results_errno != 0) {
+        $out->results_reason(); exit (-2);
+    }
+    $out = $ncs->set_admin_user($optuser, $optpasswd);
+    if (ref ($out) eq "NaElement" && $out->results_errno != 0) {
+        $out->results_reason(); exit (-2);
+    }
+    $out = $ncs->set_timeout(300);
+    if (ref ($out) eq "NaElement" && $out->results_errno != 0) {
+        $out->results_reason(); exit (-2);
+    }
+    return $ncs;
+}
+#------------------------------------------------------------------#
+# Just for testing HTTP/HTTPS Connection
+sub getversions {
+    my $cluster = shift;
+    my $ss      = connect_netapp($cluster);
+    my $out     = $ss->invoke( "system-get-version");
+    print $out->sprintf();
 }
 #------------------------------------------------------------------#
 sub lifstatloop {
-    my $filer = shift;
+    my $cluster = shift;
     my (%lifdataA, %lifdataB);
-    while ($filer) {
-        %lifdataA = getlifstat($filer);
+    while ($cluster) {
+        %lifdataA = getlifstat($cluster);
         if (!%lifdataB) {
             %lifdataB = %lifdataA;
         } else {
@@ -114,10 +129,10 @@ sub lifstatloop {
                 }
             }
             system("clear");
-            printf "%-8s  %5s  %12s %5s %8s %12s %5s %8s  %-20s \n",
+            printf "%-14s  %5s  %12s %5s %8s %12s %5s %8s  %-20s \n",
                 "Node", "UUID", "R-Data", "R-Err", "R-Pkts", "S-Data", "S-Err", "S-Pkts", "LIF-Name";
             foreach my $i (sort {$data{$b}->[3] <=> $data{$a}->[3]} keys %data) {
-                printf "%-8s  %5s  %12s %5s %8s %12s %5s %8s  %-20s \n",
+                printf "%-14s  %5s  %12s %5s %8s %12s %5s %8s  %-20s \n",
                         $data{$i}[2], $data{$i}[1], $data{$i}[3], $data{$i}[4], $data{$i}[5],
                         $data{$i}[6], $data{$i}[7], $data{$i}[8], $data{$i}[0];
             }
@@ -128,8 +143,7 @@ sub lifstatloop {
 }
 #------------------------------------------------------------------#
 sub getliflist {
-    my $filer = shift;
-	
+    my $cluster = shift;
 	my $api = new NaElement('perf-object-instance-list-info-iter');
         $api->child_add_string('objectname','lif');
         $api->child_add_string('max-records','100000');
@@ -138,13 +152,11 @@ sub getliflist {
         } elsif ($node) {
             $api->child_add_string('filter-data', "node_name=$node");
         }
-    
-    my $ss = connect_netapp($filer);
+    my $ss = connect_netapp($cluster);
 	my $out = $ss->invoke_elem($api);
     if ($out->results_status() eq "failed"){
         print ((caller(0))[3], " ", $out->results_reason(), "\n");
     }
-
     my %liflist;
 	my $instances = $out->child_get("attributes-list");
 	my @instances = $instances->children_get();
@@ -156,8 +168,8 @@ sub getliflist {
 }
 #------------------------------------------------------------------# 
 sub getlifstat{
-    my $filer = shift;
-    my %liflist = getliflist($filer);
+    my $cluster = shift;
+    my %liflist = getliflist($cluster);
 
 	my $api = new NaElement('perf-object-get-instances');
         $api->child_add_string('objectname','lif');
@@ -167,12 +179,11 @@ sub getlifstat{
         }
 	$api->child_add($apiA);    
 
-    my $ss = connect_netapp($filer);
+    my $ss = connect_netapp($cluster);
 	my $out = $ss->invoke_elem($api);
 	if ($out->results_status() eq "failed"){
         print ((caller(0))[3], " ", $out->results_reason(), "\n");
     }
-    
     my $timestamp = $out->child_get_string("timestamp");
 	my @instances = $out->child_get("instances")->children_get();
     my %lifdata;
